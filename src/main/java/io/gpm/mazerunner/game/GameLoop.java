8e609c90f43336544deefe00e539947fd5f1bf6f
George <george.marr.cobc@gmail.com>
2019-06-18 15:53:16 +0100

    add in the game loop

--- src/main/java/io/gpm/mazerunner/game/GameLoop.java
+++ src/main/java/io/gpm/mazerunner/game/GameLoop.java
@@ -2,22 +2,44 @@ package io.gpm.mazerunner.game;
 
 import io.gpm.mazerunner.MazeRunner;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 /***
  * @author George
  * @since 18-Jun-19
  */
-public class GameLoop extends BukkitRunnable {
+public class GameLoop {
 
-    private static GameLoop get;
-    private long period, delay; //todo assign this to the value later
-    @Override
-    public void run() {
-        get = this;
-        runTaskTimer(MazeRunner.getInstance(), delay, period);
-    }
+    private long length = 6000; //5 minutes -  think
+    private long delay;
+    private boolean started = false;
+    private int loopTime = 6000; //5 minutes - think
+    private BukkitTask runnable;
+    private boolean gameEnded = false;
 
-    public static GameLoop get() {
-        return get;
+    public GameLoop(long length, long delay) {
+        this.length = length;
+        this.delay = delay;
+
+        runnable = new BukkitRunnable() {
+            @Override
+            public void run() {
+                while(loopTime != 0) {
+                    --loopTime;
+
+                    if(loopTime == 0) {
+                        gameEnded = true;
+                        //todo send the end game stuff
+
+
+                    }
+                }
+
+                if(gameEnded) {
+                    cancel();
+                    //todo teleport players
+                }
+            }
+        }.runTaskTimer(MazeRunner.getInstance(), delay, length);
     }
 }
