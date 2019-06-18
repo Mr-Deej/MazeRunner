a87f84a14a0aabc7e72ac08919bde616c45bc4a4
George <george.marr.cobc@gmail.com>
2019-06-18 13:43:30 +0100

    re-init

---
+++ src/main/java/io/gpm/mazerunner/MazeRunner.java
@@ -0,0 +1,20 @@
+package io.gpm.mazerunner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/***
+ * @author George
+ * @since 18-Jun-19
+ */
+public class MazeRunner extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
