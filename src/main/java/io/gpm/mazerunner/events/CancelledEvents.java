35f892a60952b12a69abf3c8c10ae9fe3ffa0797
George <george.marr.cobc@gmail.com>
2019-06-19 00:30:04 +0100

    cancel more events

--- src/main/java/io/gpm/mazerunner/events/CancelledEvents.java
+++ src/main/java/io/gpm/mazerunner/events/CancelledEvents.java
@@ -3,7 +3,10 @@ package io.gpm.mazerunner.events;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
 
 /***
  * @author George
@@ -15,5 +18,20 @@ public class CancelledEvents implements Listener {
     public void onDamage(EntityDamageEvent event) {
         if(event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK && event.getEntity() instanceof Player)
             event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPlace(BlockPlaceEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onWeatherChange(WeatherChangeEvent event) {
+        event.setCancelled(true);
     }
 }
