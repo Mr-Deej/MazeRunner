37ad96492fc471dd0d93e29f314613af48ebbd29
George <george.marr.cobc@gmail.com>
2019-06-18 14:02:55 +0100

    include the data watcher

--- src/main/java/io/gpm/mazerunner/utils/BossBar.java
+++ src/main/java/io/gpm/mazerunner/utils/BossBar.java
@@ -1,8 +1,6 @@
 package io.gpm.mazerunner.utils;
 
-import net.minecraft.server.v1_8_R3.EntityEnderDragon;
-import net.minecraft.server.v1_8_R3.PlayerConnection;
-import net.minecraft.server.v1_8_R3.WorldServer;
+import net.minecraft.server.v1_8_R3.*;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
@@ -29,5 +27,17 @@ public class BossBar {
         //spawn the ender dragon and set its location so that it's invisible
         EntityEnderDragon dragon = new EntityEnderDragon(world);
         dragon.setLocation(location.getX(), location.getY() - 100, location.getZ(), 0, 0);
+
+        //spawn the dragon through packets for the player
+        PacketPlayOutSpawnEntityLiving dragonPacket = new PacketPlayOutSpawnEntityLiving(dragon);
+
+        //run the data watcher - this is what sets the health and the text
+        DataWatcher watcher = new DataWatcher(dragon);
+        watcher.a(0, (byte) 0x20);
+        watcher.a(6, (health * 200) / 100);
+        watcher.a(10, text);
+        watcher.a(2, text);
+        watcher.a(11, (byte) 1);
+        watcher.a(3, (byte) 1);
     }
 }
