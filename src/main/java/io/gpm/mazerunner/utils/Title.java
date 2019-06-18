d245692aa37c8cb4f3761c21244236f0ddab7f84
George <george.marr.cobc@gmail.com>
2019-06-18 13:52:49 +0100

    include the title util

---
+++ src/main/java/io/gpm/mazerunner/utils/Title.java
@@ -0,0 +1,40 @@
+package io.gpm.mazerunner.utils;
+
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+/***
+ * @author George
+ * @since 18-Jun-19
+ */
+public class Title {
+
+    private String title;
+    private String subtitle;
+    private int fadeIn, stay, fadeOut;
+
+    public Title(String title, String subtitle, int fadeIn, int stay, int fadeOut) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.fadeIn = fadeIn;
+        this.stay = stay;
+        this.fadeOut = fadeOut;
+    }
+
+    public void send(Player player) {
+        IChatBaseComponent titleCb = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + title + "\"}");
+        IChatBaseComponent subtitleCb = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + title + "\"}");
+
+        //packet handling
+        PacketPlayOutTitle titlePacket = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, titleCb);
+        PacketPlayOutTitle subtitlePacket = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, subtitleCb);
+        PacketPlayOutTitle length = new PacketPlayOutTitle(fadeIn, stay, fadeOut);
+
+        //send to the player
+        (((CraftPlayer)player).getHandle()).playerConnection.sendPacket(titlePacket);
+        (((CraftPlayer)player).getHandle()).playerConnection.sendPacket(subtitlePacket);
+        (((CraftPlayer)player).getHandle()).playerConnection.sendPacket(length);
+    }
+}
