f708e526de27b77076a5fe1f845a57edc39371a8
George <george.marr.cobc@gmail.com>
2019-06-20 13:20:49 +0100

    add in the time conversion

--- src/main/java/io/gpm/mazerunner/utils/TimeUtils.java
+++ src/main/java/io/gpm/mazerunner/utils/TimeUtils.java
@@ -5,4 +5,22 @@ package io.gpm.mazerunner.utils;
  * @since 20-Jun-19
  */
 public class TimeUtils {
+
+    public static String convertTime(int time) {
+        int sec = Integer.parseInt(String.valueOf(time), 10);
+        int remainder = sec % 86400;
+        int days = (int) Math.floor(sec / 86400);
+        int hours = (int) Math.floor(remainder / 3600);
+        int minutes = (int) Math.floor((remainder / 60) - (hours * 60));
+        int seconds = (int) Math.floor((remainder % 3600) - (minutes * 60));
+
+        if(days > 0)
+            return days + "d " + hours + "h " + "m " + seconds + "s";
+        else if(hours > 0)
+            return hours + "h " + minutes + "m " + seconds + "s";
+        else if(minutes > 0)
+            return minutes + "m " + seconds + "s";
+        else
+            return seconds + "s";
+    }
 }
